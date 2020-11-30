37bb54d30a366abe0bd3751fe1927a00b491f52a
jack.spektor@gmail.com <Jack.Spektor@akqa.com>
2020-12-01 12:28:47 +1300

    improve speed

--- themes/espekdigital/assets/js/script.js
+++ themes/espekdigital/assets/js/script.js
@@ -2,7 +2,7 @@
 $(window).on('load', function () {
 	$('.preloader').fadeOut(100);
 	console.log("Initializing 3D animation...");
-	setTimeout(initializeAnimation, 300);
+	setTimeout(initializeAnimation, 700);
 });
 
 (function ($) {
